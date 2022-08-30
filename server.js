172cd1133a2809c6297a3d66f42afcdf350eef82
Luis Angel <pezu.luis01@gmail.com>
2022-08-30 00:46:28 -0500

    feat:: Add meaddlewares to parse and log response

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+//modules
 const http = require('http'),
+    cors = require('cors'),
     express = require('express'),
+    logger = require('morgan'),
+
+// activate the modules
     app = express()
-    
+
+//middleware
+app.use(express.urlencoded({ extended: true}))  //parse URL body
+app.use(cors()) //indicate any origins (domain, scheme, or port) to load resources
+app.use(express.json()) //parse the incoming requests with JSON
+app.use(logger('dev')) //Log the time and status of the response
+
 //create a server object:
 const server = http.createServer(app)
 
 server.listen(8080, function (req, res) {
     console.log('Server is running on http://localhost:8080'); //write a response to the client
-}); //the server object listens on port 8080
+}); //run server on port 8080
