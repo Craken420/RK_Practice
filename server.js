fac2090918dd3668c2691ee0f9787ad8125e5c8b
Luis Angel <pezu.luis01@gmail.com>
2022-08-30 00:50:13 -0500

    feat:: Set the port in a setting value

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.use(cors()) //indicate any origins (domain, scheme, or port) to load resourc
 app.use(express.json()) //parse the incoming requests with JSON
 app.use(logger('dev')) //Log the time and status of the response
 
+//settings values
+app.set('PORT', 8080)
+
 //create a server object
-app.listen(8080, function (req, res) {
-    console.log('Server is running on http://localhost:8080'); //write a response to the client
+app.listen(app.get('PORT'), function (req, res) {
+    console.log(`Server is running on http://localhost:${app.get('PORT')}`); //write a response to the client
 }); //run server on port 8080
