849e6eb39dfe302dd349617410461539ba1b182d
Luis Angel <pezu.luis01@gmail.com>
2022-09-04 03:09:10 -0500

    feat:: Add the strict mode

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
+'use strict'
 //modules
 const cors = require('cors'),
     express = require('express'),
     logger = require('morgan'),
-    path = require('path')
+    path = require('path'),
 
 // activate the modules
-    app = express()
+    app = express(),
     router = require('express').Router(); // export routs used as middleware
 
 //middleware
-app.use(express.urlencoded({ extended: true}))  //parse URL body
-app.use(cors()) //indicate any origins (domain, scheme, or port) to load resources
-app.use(express.json()) //parse the incoming requests with JSON
-app.use(logger('dev')) //Log the time and status of the response
+app.use(express.urlencoded({ extended: true})); //parse URL body
+app.use(cors()); //indicate any origins (domain, scheme, or port) to load resources
+app.use(express.json()); //parse the incoming requests with JSON
+app.use(logger('dev')); //Log the time and status of the response
 
 //settings values
-app.set('PORT', 8080)
+app.set('PORT', 8080);
 
 app.use(express.static('public'));
 
 //path's
 router.route('/').get(function (req, res){
-    res.sendFile(path.join(__dirname + '/index.html'))
-})
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-app.use('/', router)
+app.use('/', router);
 
 //create a server object
 app.listen(app.get('PORT'), function (req, res) {
